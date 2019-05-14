80ef90f583b382a9e81db9db0555809cfda3486b
sunwx <979362142@qq.com>
2019-05-14 14:49:13 +0800

    jenkins测试项目

---
+++ src/main/java/com/example/test1/Test.java
@@ -0,0 +1,18 @@
+package com.example.test1;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author sunwx
+ * @description:
+ * @date 2019/5/1414:48
+ */
+@RestController
+public class Test {
+
+    @RequestMapping("/index")
+    public String index(){
+        return "测试1";
+    }
+}
